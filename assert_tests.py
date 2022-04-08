b2c8cdb60433eaccbb796536ca34191e0df11e8b
c2-foonyee <choo2.foonyee@live.uwe.ac.uk>
2022-04-09 00:32:24 +0100

    added comments

--- assert_tests.py
+++ assert_tests.py
@@ -5,14 +5,16 @@ def test_encode_us():
     assert morse.encode('us') == '..- ...', "Should be ..- ..."
     assert morse.encode('FY') == '..-. -.--', "Should be ..-"
     assert morse.encode('UWE') == '..- .-- .', "Should be ..-"
-    assert morse.encode('FAIL') == '-..-. .- .. .-..', "Should be ..-"
-    assert morse.encode('FAIL2') == '-..-. .- .. .-.. ..---', "Should be ..-"
+    #fail testing
+    assert morse.encode('FAIL') == '..... ---', "Should be ..-"
+    assert morse.encode('FAIL2') == '..... ---', "Should be ..-"
 
     assert morse.decode('..- ...') == 'US', "Should be ..-"
     assert morse.decode('.. --- -') == 'IOT', "Should be ..-"
     assert morse.decode('..- .-- .') == 'UWE', "Should be ..-"
     assert morse.decode('.-.. --- .-..') == 'LOL', "Should be ..-"
-    assert morse.decode('-..-. .- .. .-..') == 'FAIL', "Should be ..-"
+    #fail testing
+    assert morse.decode('..... .. .. ....') == 'FAIL3', "Should be ..-"
 
 if __name__ == "__main__":
     #assert_tests.test_encode_us()

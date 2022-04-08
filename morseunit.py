55cf3c34d52ab0ce5116d2f511698b576aed00e8
c2-foonyee <choo2.foonyee@live.uwe.ac.uk>
2022-04-09 00:34:12 +0100

    added comments in morseunit.py

--- morseunit.py
+++ morseunit.py
@@ -1,46 +1,41 @@
 import unittest
 import morse
 
-
+#unit testing for 5 encode
 class TestMorse(unittest.TestCase):
     def test_encode_us(self):
         self.assertEqual( morse.encode('us'), '..- ...')
 
-class TestMorse(unittest.TestCase):
     def test_encode_fy(self):
         self.assertEqual( morse.encode('FY'), '..-. -.--')
 
-class TestMorse(unittest.TestCase):
     def test_encode_uwe(self):
         self.assertEqual( morse.encode('UWE'), '..- .-- .')
 
-class TestMorse(unittest.TestCase):
+    #fail test 1
     def test_encode_fail(self):
-        self.assertEqual( morse.encode('FAIL'), '-..-. .- .. .-..')
+        self.assertEqual( morse.encode('FAIL'), '..... ---')
 
-class TestMorse(unittest.TestCase):
+    #fail test 2
     def test_encode_fail2(self):
-        self.assertEqual( morse.encode('FAIL2'), '-..-. .- .. .-.. ..---')
+        self.assertEqual( morse.encode('FAIL2'), '..... ---')
 
-class TestMorse(unittest.TestCase):
+    #unit testing for 5 decode
     def test_decode_us(self):
         self.assertEqual( morse.encode('..- ...'), 'US')
 
-class TestMorse(unittest.TestCase):
     def test_decode_us(self):
         self.assertEqual( morse.encode('.. --- -'), 'IOT')
 
-class TestMorse(unittest.TestCase):
     def test_decode_us(self):
         self.assertEqual( morse.encode('..- .-- .'), 'UWE')
 
-class TestMorse(unittest.TestCase):
     def test_decode_us(self):
         self.assertEqual( morse.encode('.-.. --- .-..'), 'LOL')
 
-class TestMorse(unittest.TestCase):
+    #fail test 3
     def test_decode_us(self):
-        self.assertEqual( morse.encode('-..-. .- .. .-..'), 'FAIL')
+        self.assertEqual( morse.encode('..... .. .. ....'), 'FAIL3')
         
 
 if __name__ == '__main__':

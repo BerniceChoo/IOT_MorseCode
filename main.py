4e6035afa2b3b8909e6ecf9760133ed61ca90b72
c2-foonyee <choo2.foonyee@live.uwe.ac.uk>
2022-04-09 01:56:19 +0100

    edited __name__

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import morse
 if __name__ == "__main__":
     e = morse.encode('us')
     print('%s' % e)
-    d = morse.decode(e)
+    d = morse.decode('lol')
+    print('%s' % d)
 
     assert morse.encode('us') == '..- ...', "Should be ..-"
     assert morse.encode('FY') == '..-. -.--', "Should be ..-"
